0406f6ccc7650d257307e934c7090405554f07f8
DanielMagero <mdmagero700@gmail.com>
2024-10-23 09:45:19 +0300

    own react app

---
+++ Week_8/practice2/sunapp/app/page.js
@@ -0,0 +1,17 @@
+function Home() {
+  return (
+    <body>
+      <h1 textalign="center">The Sun App</h1>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnl-SsdZpw8cX-BdenGgYWD_SCb7gB6j7bbA&s"
+        alt="sun picture"
+      />
+      <img
+        src="Week_8/practice2/sunapp/app/images/sun1.jpeg"
+        alt="sun picture"
+      />
+    </body>
+  );
+}
+
+export default Home;
